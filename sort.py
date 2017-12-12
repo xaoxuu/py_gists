c16957ce70535622eb18b1469ba6a8e42e6f0702
xaoxuu <xaoxuu@gmail.com>
2017-12-12 19:28:07 +0800

    update

---
+++ sort.py
@@ -0,0 +1,14 @@
+
+def sort(array):
+    for index in range(1, len(array)):
+        value = array[index]
+        i = index - 1
+        while i >= 0 and (value < array[i]):
+            array[i + 1] = array[i]
+            array[i] = value
+            i -= 1
+
+
+a = [1, 3, 12, 4, 1, -5]
+sort(a)
+print(a)
